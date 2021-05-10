66db078468a706fb9ae0669550a34afd1f660954
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-05-10 17:40:37 +0200

    Added user model

---
+++ app/src/main/java/com/example/android/notebookapplication/models/User.java
@@ -0,0 +1,59 @@
+package com.example.android.notebookapplication.models;
+
+public class User {
+    private String userName;
+    private String email;
+    private String password;
+    private String name;
+    private String lastName;
+
+    private Boolean isActive;
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+}
