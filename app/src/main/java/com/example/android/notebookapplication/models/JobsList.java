b5ffa787d116d1f63a5f39629a67ea249ebb61af
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-05-10 17:50:15 +0200

    Added jobs list model

---
+++ app/src/main/java/com/example/android/notebookapplication/models/JobsList.java
@@ -0,0 +1,51 @@
+package com.example.android.notebookapplication.models;
+
+import java.sql.Date;
+
+public class JobsList {
+    private int id;
+    private String name;
+    private Date created;
+    private Date edited;
+    private int owner_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getEdited() {
+        return edited;
+    }
+
+    public void setEdited(Date edited) {
+        this.edited = edited;
+    }
+
+    public int getOwner_id() {
+        return owner_id;
+    }
+
+    public void setOwner_id(int owner_id) {
+        this.owner_id = owner_id;
+    }
+}
