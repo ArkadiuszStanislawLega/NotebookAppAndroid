648ca7c542a0041add21d6ddd85558c578c2c007
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-05-10 18:09:08 +0200

    Added login view.

--- app/src/main/java/com/example/android/notebookapplication/MainActivity.java
+++ app/src/main/java/com/example/android/notebookapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.android.notebookapplication;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private EditText edtLogin;
+    private EditText edtPass;
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +31,61 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        this.initControls();
+        this.initListeners();
     }
 
+
+    private void initControls() {
+        this.edtLogin = findViewById(R.id.edtLogin);
+        this.edtPass = findViewById(R.id.edtPass);
+        this.btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    private boolean authenticateUser() {
+        if (TextUtils.isEmpty(this.edtLogin.getText())) {
+            Toast.makeText(this,
+                    getString(R.string.login__validation_login_empty),
+                    Toast.LENGTH_SHORT)
+                    .show();
+            return false;
+        } //if
+        if (TextUtils.isEmpty(this.edtPass.getText())) {
+            Toast.makeText(this,
+                    getString(R.string.login__validation_pass_empty),
+                    Toast.LENGTH_SHORT)
+                    .show();
+            return false;
+        } //if
+        if (this.isLoginValid() && this.isPasswordValid()) {
+            return true;
+        } //if
+        return false;
+    }
+
+    private void initListeners() {
+        if (this.btnLogin != null) {
+            this.btnLogin.setOnClickListener(view -> {
+                if (this.authenticateUser()) {
+                    Toast.makeText(this,
+                            getString(R.string.login__success),
+                            Toast.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                Toast.makeText(this,
+                        getString(R.string.login__failure),
+                        Toast.LENGTH_SHORT)
+                        .show();
+            });
+        } //if
+    }
+
+    private boolean isLoginValid(){
+        return this.edtLogin.getText().toString().equals("admin");
+    }
+
+    private boolean isPasswordValid(){
+        return this.edtPass.getText().toString().equals("admin");
+    }
 }
