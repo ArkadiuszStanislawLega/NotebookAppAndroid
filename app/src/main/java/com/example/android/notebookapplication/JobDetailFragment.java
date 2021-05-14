0864c2acf68210e9ba139fd7ba65df68b369dd44
ArkadiuszStanislaLega <arkadiusz.stanislaw.lega@gmail.com>
2021-05-14 12:47:19 +0200

    Update job detail fragment - added switch is finished

--- app/src/main/java/com/example/android/notebookapplication/JobDetailFragment.java
+++ app/src/main/java/com/example/android/notebookapplication/JobDetailFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.example.android.notebookapplication.models.Job;
@@ -23,6 +24,7 @@ public class JobDetailFragment extends Fragment {
     private TextView _tvContent;
     private TextView _tvCreated;
     private TextView _tvEdited;
+    private Switch _sIsFinished;
     public static JobDetailFragment newInstance() {
         return new JobDetailFragment();
     }
@@ -46,11 +48,12 @@ public class JobDetailFragment extends Fragment {
             this._tvContent = this._currentView.findViewById(R.id.job_detail_content);
             this._tvCreated = this._currentView.findViewById(R.id.job_created_date);
             this._tvEdited = this._currentView.findViewById(R.id.job_edited_date);
-
+            this._sIsFinished = this._currentView.findViewById(R.id.job_is_finished);
             this._tvTitle.setText(this._job.getTitle());
             this._tvContent.setText(this._job.getContent());
             this._tvCreated.setText(created);
             this._tvEdited.setText(edited);
+            this._sIsFinished.setChecked(this._job.isFinished());
 
 
         } catch (NullPointerException e){
