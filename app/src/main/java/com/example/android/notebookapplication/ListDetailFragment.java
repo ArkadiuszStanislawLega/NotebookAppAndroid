ccccad172438b932922c8d6bd2047f313e62d6e0
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-05-15 09:35:14 +0200

    Update list detail view - added edit text to edit name

--- app/src/main/java/com/example/android/notebookapplication/ListDetailFragment.java
+++ app/src/main/java/com/example/android/notebookapplication/ListDetailFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,14 +22,15 @@ import java.text.SimpleDateFormat;
 public class ListDetailFragment extends Fragment {
     private boolean _isEditModeOn = false;
     private JobsList _jobsList;
-    private TextView _tvListName;
-    private TextView _tvListEditedDate;
-    private TextView _tvListCreatedDate;
-    private FloatingActionButton _fabAdd;
-    private FloatingActionButton _fabEdit;
-    private FloatingActionButton _fabDelete;
-    private FloatingActionButton _fabConfirmEdit;
-    private FloatingActionButton _fabCancelEdit;
+    private TextView _tvListName,
+            _tvListEditedDate,
+            _tvListCreatedDate;
+    private EditText _etListName;
+    private FloatingActionButton _fabAdd,
+            _fabEdit,
+            _fabDelete,
+            _fabConfirmEdit,
+            _fabCancelEdit;
     private LinearLayout _llEditButtons;
     private View _currentView;
 
@@ -47,7 +49,7 @@ public class ListDetailFragment extends Fragment {
             this.setValuesToControls();
             this.setListeners();
 
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println(e);
         }
 
@@ -62,10 +64,11 @@ public class ListDetailFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
-    private void initControls(){
+    private void initControls() {
         this._tvListName = this._currentView.findViewById(R.id.list_name);
         this._tvListCreatedDate = this._currentView.findViewById(R.id.list_created_date);
         this._tvListEditedDate = this._currentView.findViewById(R.id.list_edited_date);
+        this._etListName = this._currentView.findViewById(R.id.et_list_name);
         this._fabAdd = this._currentView.findViewById(R.id.add_job);
         this._fabEdit = this._currentView.findViewById(R.id.edit_list);
         this._fabDelete = this._currentView.findViewById(R.id.delete_list);
@@ -74,7 +77,7 @@ public class ListDetailFragment extends Fragment {
         this._llEditButtons = this._currentView.findViewById(R.id.edit_buttons);
     }
 
-    private void setListeners(){
+    private void setListeners() {
         this._fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -116,28 +119,32 @@ public class ListDetailFragment extends Fragment {
         });
     }
 
-    private void showEditMode(){
-        if (!_isEditModeOn){
+    private void showEditMode() {
+        if (!_isEditModeOn) {
             this._llEditButtons.setVisibility(View.VISIBLE);
+            this._etListName.setVisibility(View.VISIBLE);
+            this._tvListName.setVisibility(View.GONE);
             this._fabEdit.setVisibility(View.GONE);
             this._isEditModeOn = true;
-        }
-        else{
+        } else {
             this._llEditButtons.setVisibility(View.GONE);
+            this._etListName.setVisibility(View.GONE);
+            this._tvListName.setVisibility(View.VISIBLE);
             this._fabEdit.setVisibility(View.VISIBLE);
             this._isEditModeOn = false;
         }
     }
 
-    private void setValuesToControls(){
-        String DATE_FORMAT = getString(R.string.date_format) ;
+    private void setValuesToControls() {
+        String DATE_FORMAT = getString(R.string.date_format);
         String TIME_FORMAT = getString(R.string.time_format);
 
-        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT+ " " + TIME_FORMAT);
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT + " " + TIME_FORMAT);
         String edited = formatter.format(_jobsList.getEdited());
         String created = formatter.format(_jobsList.getCreated());
 
-        this._tvListName.setText(""+this._jobsList.getName());
+        this._etListName.setText("" + this._jobsList.getName());
+        this._tvListName.setText("" + this._jobsList.getName());
         this._tvListEditedDate.setText(edited);
         this._tvListCreatedDate.setText(created);
     }
