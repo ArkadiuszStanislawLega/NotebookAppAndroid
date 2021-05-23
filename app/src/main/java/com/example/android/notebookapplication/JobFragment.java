d37d3261015529a8b6575ea074e35394a16bd752
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-05-23 16:28:06 +0200

    Code review - model view and job fragment.

--- app/src/main/java/com/example/android/notebookapplication/JobFragment.java
+++ app/src/main/java/com/example/android/notebookapplication/JobFragment.java
@@ -26,9 +26,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-/**
- * A fragment representing a list of Items.
- */
 public class JobFragment extends Fragment {
 
     private View _currentView;
@@ -36,34 +33,17 @@ public class JobFragment extends Fragment {
     private EditText _etJobName;
     private Button _bAddJob;
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public JobFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static JobFragment newInstance(int columnCount) {
+    public static JobFragment newInstance() {
         JobFragment fragment = new JobFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
@@ -75,13 +55,6 @@ public class JobFragment extends Fragment {
         initListener();
 
         if (this._rvList instanceof RecyclerView) {
-            Context context = this._currentView.getContext();
-
-            if (mColumnCount <= 1) {
-                this._rvList.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                this._rvList.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
             this._rvList.setAdapter(new JobsRecyclerViewAdapter(LoggedInActivity.viewModel.getSelectedList().get_jobs()));
         }
         return this._currentView;
--- app/src/main/java/com/example/android/notebookapplication/models/ListsViewModel.java
+++ app/src/main/java/com/example/android/notebookapplication/models/ListsViewModel.java
@@ -5,16 +5,15 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.android.notebookapplication.Database.NotebookDatabase;
-import com.example.android.notebookapplication.LoggedInActivity;
 
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ListsViewModel extends ViewModel {
-    private NotebookDatabase _database;
     private final int DATABASE_TIMEOUT = 100;
     private long _userId;
+
     private MutableLiveData<List<JobsList>> _lists = new MutableLiveData<List<JobsList>>();
 
     private List<JobsList> _tempList;
@@ -22,6 +21,8 @@ public class ListsViewModel extends ViewModel {
 
     private JobsList _selectedList;
     private Job _selectedJob;
+
+    private NotebookDatabase _database;
 
     public ListsViewModel() {
         this.getLists();
@@ -66,85 +67,98 @@ public class ListsViewModel extends ViewModel {
     }
 
     private void loadLists(){
-        this._database.getQueryExecutor().execute(() -> {
-            _tempList = this._database.jobsListDAO().getUserWithLists(this._userId);
-        });
-        try {
-            TimeUnit.MILLISECONDS.sleep(DATABASE_TIMEOUT);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (this._userId > 0 && this._database != null) {
+            this._database.getQueryExecutor().execute(() -> {
+                _tempList = this._database.jobsListDAO().getUserWithLists(this._userId);
+            });
+            try {
+                TimeUnit.MILLISECONDS.sleep(DATABASE_TIMEOUT);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this._lists.setValue(this._tempList);
         }
-        this._lists.setValue(this._tempList);
     }
 
     private void loadJobs(){
-        this._selectedList.set_jobs(null);
-        this._database.getQueryExecutor().execute(() -> {
-            _tempJobs = this._database.jobDAO().getJobsList(this._selectedList.get_jobsListId());
-        });
-        try {
-            TimeUnit.MILLISECONDS.sleep(DATABASE_TIMEOUT);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (this._selectedList != null && this._database != null) {
+            this._selectedList.set_jobs(null);
+            this._database.getQueryExecutor().execute(() -> {
+                _tempJobs = this._database.jobDAO().getJobsList(this._selectedList.get_jobsListId());
+            });
+            try {
+                TimeUnit.MILLISECONDS.sleep(DATABASE_TIMEOUT);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            this._selectedList.set_jobs(this._tempJobs);
         }
-        this._selectedList.set_jobs(this._tempJobs);
     }
 
     public void addList(JobsList jobsList){
+        if (jobsList != null && this._database != null) {
+            jobsList.set_owner_id(this._userId);
+            jobsList.set_created(new Date());
+            jobsList.set_edited(new Date());
 
-        jobsList.set_owner_id(this._userId);
-        jobsList.set_created(new Date());
-        jobsList.set_edited(new Date());
-
-        this._database.getQueryExecutor().execute(() -> {
-            this._database.jobsListDAO().insert(jobsList);
-        });
-        loadLists();
+            this._database.getQueryExecutor().execute(() -> {
+                this._database.jobsListDAO().insert(jobsList);
+            });
+            loadLists();
+        }
     }
 
     public void removeList(){
-        _database.getQueryExecutor().execute(() -> {
-            _database.jobsListDAO().delete(this._selectedList);
-        });
-        this._selectedJob = null;
-        loadLists();
+        if (this._selectedList != null && this._database != null) {
+            _database.getQueryExecutor().execute(() -> {
+                _database.jobsListDAO().delete(this._selectedList);
+            });
+            this._selectedJob = null;
+            loadLists();
+        }
     }
 
     public void addJob(Job job){
-        job.set_parentId(this._selectedList.get_jobsListId());
-        job.set_created(new Date());
-        job.set_edited(new Date());
+        if (job != null && this._selectedList != null && this._database != null) {
+            job.set_parentId(this._selectedList.get_jobsListId());
+            job.set_created(new Date());
+            job.set_edited(new Date());
 
-        this._selectedList.set_edited(new Date());
+            this._selectedList.set_edited(new Date());
 
-        this._database.getQueryExecutor().execute(() -> {
-            this._database.jobDAO().insert(job);
-            this._database.jobsListDAO().update(this._selectedList);
-        });
+            this._database.getQueryExecutor().execute(() -> {
+                this._database.jobDAO().insert(job);
+                this._database.jobsListDAO().update(this._selectedList);
+            });
 
-        loadLists();
-        loadJobs();
+            loadLists();
+            loadJobs();
+        }
     }
 
     public void removeJob(){
-        this._selectedList.set_edited(new Date());
+        if (this._selectedJob != null && this._selectedList != null && this._database != null) {
+            this._selectedList.set_edited(new Date());
 
-        _database.getQueryExecutor().execute(() -> {
-            _database.jobDAO().delete(this._selectedJob);
-            _database.jobsListDAO().update(this._selectedList);
-        });
-        loadLists();
-        loadJobs();
-        this._selectedJob = null;
+            _database.getQueryExecutor().execute(() -> {
+                _database.jobDAO().delete(this._selectedJob);
+                _database.jobsListDAO().update(this._selectedList);
+            });
+            loadLists();
+            loadJobs();
+            this._selectedJob = null;
+        }
     }
 
     public void updateJob(){
-        this._selectedList.set_edited(new Date());
-        this._selectedJob.set_edited(new Date());
+        if( this._selectedJob != null && this._selectedList != null && this._database != null) {
+            this._selectedList.set_edited(new Date());
+            this._selectedJob.set_edited(new Date());
 
-        _database.getQueryExecutor().execute(() -> {
-            _database.jobDAO().update(this._selectedJob);
-            _database.jobsListDAO().update(this._selectedList);
-        });
+            _database.getQueryExecutor().execute(() -> {
+                _database.jobDAO().update(this._selectedJob);
+                _database.jobsListDAO().update(this._selectedList);
+            });
+        }
     }
 }
